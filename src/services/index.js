9e9fa7e78375557d3a850210ae143197b60b39fb
Shpakova Alena <shpakovadev@gmail.com>
2020-03-15 16:54:29 +0300

    add api class

---
+++ src/services/index.js
@@ -0,0 +1,16 @@
+export default class tagService {
+  _nameUrl = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag"
+  getAnswer = async url => {
+    const res = await fetch(`${this._nameUrl}${url}`)
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`)
+    }
+    return await res.json()
+  }
+
+  getImg = async tag => {
+    const body = await this.getAnswer(`=${tag}`)
+    const url = body.data.image_url
+    return url
+  }
+}
