9fdd289aff1fbd636bb91034cb0bd6990f7c6a97
Shpakova Alena <shpakovadev@gmail.com>
2020-03-15 16:53:12 +0300

    add app component and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react"
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    tag: "",
+    images: [],
+    isStartGetImg: false,
+    isLoading: false,
+    isGrouped: false,
+    isNotFindTag: false,
+    err: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="form">
+          <div className="action">
+            <input
+              value={this.state.tag}
+              type="text"
+              className="form-control"
+              placeholder="введите тег"
+            />
+            <button type="button" className="btn btn-primary">
+              Загрузить
+            </button>
+
+            <button type="button" className="btn btn-danger">
+              Очистить
+            </button>
+            <button type="button" className="btn btn-warning">
+              Сгруппировать
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
